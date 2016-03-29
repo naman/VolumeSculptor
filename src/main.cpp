05fcc03047123e32af1e0272b4bbabc5ef36053c
Naman <namanspace@gmail.com>
2016-03-29 15:41:28 +0530

    glitch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
-#include "SDT_Octree.h"
 #include "implicit_sphere.h"
-#include "octree_2.h"
-#include "ImplicitPolyDataDistance.h"
-#include "octree_render.h"
-#include "marching_sample.h"
 
 int main() {
 
@@ -11,7 +6,6 @@ int main() {
      implicit_sphere();
 //     marching_sample();
 //     octree_2();
-//     octree_3D();
 //     octree_render();
 
 //    SDT_Octree();
