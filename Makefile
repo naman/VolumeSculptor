489341f915aee50fba66a4b6111fa2d28d7d604d
Naman <namanspace@gmail.com>
2016-04-03 22:43:56 +0530

    GLFW files added

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 all:
-	g++ -pthread -O3 -DNDEBUG -I . -I .. -isystem /usr/include -isystem /usr/include -isystem /usr/include -isystem /usr/include -DOPENVDB_USE_LOG4CPLUS -o bin/levelset src/levelset.cpp -I . \
+#	g++ -pthread -O3 -DNDEBUG -I . -I .. -isystem /usr/include -isystem /usr/include -isystem /usr/include -isystem /usr/include -DOPENVDB_USE_LOG4CPLUS -o bin/levelset src/levelset.cpp -I . \
+#	    -Wl,-rpath,$(HOME)/Documents/openvdb/bin/lib -L $(HOME)/Documents/openvdb libopenvdb.so.3.1.0 \
+#	    -ldl -lm -lz -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lHalf -Wl,-rpath,/usr/lib -L/usr/lib -ltbb -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lboost_iostreams -lboost_system  -Wl,-rpath,/usr/lib -L/usr/lib -llog4cplus -Wl,-rpath,/usr/lib -L/usr/lib -lrt -ltbbmalloc_proxy -ltbbmalloc
+	
+	g++ -pthread -O3 -DNDEBUG -I . -I .. -isystem /usr/include -isystem /usr/include -isystem /usr/include -isystem /usr/include -DOPENVDB_USE_LOG4CPLUS -o bin/main src/main.cpp -I . \
 	    -Wl,-rpath,$(HOME)/Documents/openvdb/bin/lib -L $(HOME)/Documents/openvdb libopenvdb.so.3.1.0 \
 	    -ldl -lm -lz -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lHalf -Wl,-rpath,/usr/lib -L/usr/lib -ltbb -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lboost_iostreams -lboost_system  -Wl,-rpath,/usr/lib -L/usr/lib -llog4cplus -Wl,-rpath,/usr/lib -L/usr/lib -lrt -ltbbmalloc_proxy -ltbbmalloc
 	
-	vdb_view mygrids.vdb
+#	vdb_view new_grids.vdb
 
 clean:
-	rm bin/levelset mygrids.vdb
+	rm bin/levelset grids.vdb new_grids.vdb
--- src/levelset.cpp
+++ src/levelset.cpp
@@ -5,77 +5,96 @@
 #include <openvdb/math/Vec3.h>
 #include <openvdb/openvdb.h>
 #include <openvdb/tools/LevelSetSphere.h>
+#include <openvdb/tree/TreeIterator.h>
 #include <openvdb/Types.h>
 #include <vector>
 
 using namespace openvdb;
 
-void createAndWriteGrid()
-{
-    float backgroundValue = 2.0;
-    FloatGrid::Ptr grid = FloatGrid::create(backgroundValue);
+void createAndWriteGrid() {
+	float backgroundValue = 2.0;
+	FloatGrid::Ptr grid = FloatGrid::create(backgroundValue);
 
-    CoordBBox indexBB(Coord(-20, -20, -20), Coord(20, 20, 20));
+	CoordBBox indexBB(Coord(-20, -20, -20), Coord(20, 20, 20));
 
-    grid =   tools::createLevelSetSphere<FloatGrid>(
-            /*radius=*/50.0, /*center=*/ Vec3f(1.5, 2, 3),
-            /*voxel size=*/0.5, /*width=*/4.0);
+	grid = tools::createLevelSetSphere<FloatGrid>(
+	/*radius=*/100.0, /*center=*/Vec3f(1.5, 2, 3),
+	/*voxel size=*/1.5, /*width=*/2.0);
 
-    grid->setName("LevelSetSphere");
+	grid->setName("LevelSetSphere");
 
-    io::File file("mygrids.vdb");
+	io::File file("grids.vdb");
 
-    GridPtrVec grids;
-    grids.push_back(grid);
+	GridPtrVec grids;
+	grids.push_back(grid);
 
-    file.write(grids);
-    file.close();
+	file.write(grids);
+	file.close();
 }
 
+void modifyGrid() {
+	io::File file("grids.vdb");
+	file.open();
 
+	GridBase::Ptr baseGrid;
+	baseGrid = file.readGrid("LevelSetSphere");
 
-int main()
-{
-    // Initialize the OpenVDB library.  This must be called at least
-    // once per program and may safely be called multiple times.
-    initialize();
-    // Create an empty floating-point grid with background value 0.
-    createAndWriteGrid();
+	file.close();
 
+	// "LevelSetSphere" is a FloatGrid,
+	// so cast the generic grid pointer to a FloatGrid pointer.
+	FloatGrid::Ptr grid = gridPtrCast<FloatGrid>(baseGrid);
+
+	// Convert the level set sphere to a narrow-band fog volume, in which
+	// interior voxels have value 1, exterior voxels have value 0, and
+	// narrow-band voxels have values varying linearly from 0 to 1.
+
+	float outside = grid->background(); //2
+	float width = 2.0 * outside;       //4. Why are we doing this?
+
+	// Visit and update all of the grid's active values, which correspond to
+	// voxels on the narrow band.
+	// This can be used over the timestep to update the active voxels in narrow
+	// band and apply force according to the equation.
+	for (FloatGrid::ValueOnIter iter = grid->beginValueOn(); iter; ++iter) {
+		float dist = iter.getValue();
+		iter.setValue((outside - dist) / width); //((2-dist)/4
+	}
+
+	// Visit all of the grid's inactive tile and voxel values and update the values
+	// that correspond to the interior region.
+	for (FloatGrid::ValueOffIter iter = grid->beginValueOff(); iter; ++iter) {
+		if (iter.getValue() < 0.0) {
+			iter.setValue(1.0);
+			iter.setValueOff();
+		}
+		if (iter.getValue() > 0.0) {
+			iter.setValue(0.0);
+			iter.setValueOff();
+		}
+	}
+	// Set exterior voxels to 0.
+//	grid->setBackground(0.0);
+
+//	grid->setName("LevelSetSphere");
+
+	io::File output_file("new_grids.vdb");
+
+	GridPtrVec grids;
+	grids.push_back(grid);
+
+	output_file.write(grids);
+	output_file.close();
 }
 
+int main() {
+	// Initialize the OpenVDB library.  This must be called at least
+	// once per program and may safely be called multiple times.
+	initialize();
+	// Create an empty floating-point grid with background value 0.
+//	createAndWriteGrid();
 
+	modifyGrid();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
