175dd50d59adcc4dbd23f21c4a2b8ff29ad7fb14
Naman <namanspace@gmail.com>
2016-04-05 15:57:31 +0530

    force functions

--- src/levelset.cpp
+++ src/levelset.cpp
@@ -4,15 +4,30 @@
 #include <openvdb/math/Coord.h>
 #include <openvdb/math/Vec3.h>
 #include <openvdb/openvdb.h>
-#include <openvdb/tools/LevelSetRebuild.h>
 #include <openvdb/tools/LevelSetSphere.h>
 #include <openvdb/tree/TreeIterator.h>
 #include <openvdb/Types.h>
-#include <stddef.h>
 #include <vector>
+
+#include "../../../../Documents/openvdb/viewer/Viewer.h"
 
 using namespace openvdb;
 using namespace std;
+using namespace openvdb_viewer;
+
+float constantForce() {
+	//Force in Newtons
+	float k = -10;
+	return k;
+}
+
+float gaussianForce() {
+	/// For bumps, inside and outside ///
+	float sigma = 1;
+//	F = exp −||p−p 0 ||/2σ	2
+	float k = 1;
+	return k;
+}
 
 void createAndWriteGrid() {
 	float backgroundValue = 2.0;
@@ -22,7 +37,7 @@ void createAndWriteGrid() {
 
 	grid = tools::createLevelSetSphere<FloatGrid>(
 	/*radius=*/100.0, /*center=*/Vec3f(1.5, 2, 3),
-	/*voxel size=*/1.5, /*width=*/5.0);
+	/*voxel size=*/1.5, /*width=*/0.5); //METERS
 
 	grid->setName("LevelSetSphere");
 
@@ -47,17 +62,9 @@ void modifyGrid() {
 	// so cast the generic grid pointer to a FloatGrid pointer.
 	FloatGrid::Ptr grid = gridPtrCast<FloatGrid>(baseGrid);
 
-	// Convert the level set sphere to a narrow-band fog volume, in which
-	// interior voxels have value 1, exterior voxels have value 0, and
-	// narrow-band voxels have values varying linearly from 0 to 1.
-
-	float outside = grid->background(); //2
-	float width = 2.0 * outside;       //4. width of narrow band
-
 	// Visit and update all of the grid's active values, which correspond to
 	// voxels on the narrow band.
-	// This can be used over the timestep to update the active voxels in narrow
-	// band and apply force according to the equation.
+
 	math::Coord coord;
 	coord.setX(41);
 	coord.setY(56);
@@ -70,40 +77,25 @@ void modifyGrid() {
 
 	math::Coord coord_;
 
-
-	float k = -45;
+	float F = constantForce();
+	float F = gaussianForce();
 
 	for (FloatGrid::ValueOnIter iter = grid->beginValueOn(); iter; ++iter) {
 		float dist = iter.getValue();
 		coord_ = iter.getCoord();
 		if (coord_ > coord && coord_ < coord1) {
-//			coords.push_back(coord_);
-//			cout << dist << endl;
-//			values.push_back(dist);
-			iter.setValue(dist - k);
-			//((2-dist)/4
-			//			cout << coord_.x() <<" " << coord_.y() <<" "<< coord_.z() << endl;
+			iter.setValue(dist - F);
 		}
 	}
 
-//	tools::levelSetRebuild(*grid, 0.0, 0.5); //Is not doing anything :/
+	/*
+	 x[0] = initial_narrow_band_voxel-value;
+	 for (t = 0; t < 100; ++t) { 100s
+	 x[t] = x[t-1]  - F;
 
-	// Visit all of the grid's inactive tile and voxel values and update the values
-	// that correspond to the interior region.
-	for (FloatGrid::ValueOffIter iter = grid->beginValueOff(); iter; ++iter) {
-		if (iter.getValue() < 0.0) {
-			iter.setValue(-1 * width);
-			iter.setValueOff();
-		}
-		if (iter.getValue() > 0.0) {
-			iter.setValue(width);
-			iter.setValueOff();
-		}
-	}
-	// Set exterior voxels to 0.
-//	grid->setBackground(0.0);
-
-//	grid->setName("LevelSetSphere");
+	 //construct a grid, display it.
+	 }
+	 */
 
 	io::File output_file("new_grids.vdb");
 
@@ -114,6 +106,25 @@ void modifyGrid() {
 	output_file.close();
 }
 
+void displayGrid() {
+	openvdb_viewer::Viewer viewer = openvdb_viewer::init("Test", /*bg=*/
+	false);
+
+	GridCPtrVec allGrids;
+
+	// Load VDB files.
+	io::File file("new_grids.vdb");
+	file.open();
+
+	GridPtrVecPtr grids = file.getGrids();
+	allGrids.insert(allGrids.end(), grids->begin(), grids->end());
+
+	viewer.open();
+	viewer.view(allGrids);
+
+	openvdb_viewer::exit();
+}
+
 int main() {
 	// Initialize the OpenVDB library.  This must be called at least
 	// once per program and may safely be called multiple times.
@@ -121,7 +132,7 @@ int main() {
 	// Create an empty floating-point grid with background value 0.
 //	createAndWriteGrid();
 
-	modifyGrid();
-
+//	modifyGrid();
+	displayGrid();
 }
 
