7685f2a8355baa496070615a16df864473f8e6ba
Naman <namanspace@gmail.com>
2016-04-09 12:58:00 +0530

    Creating a Velocity Grid and levelsetAdvection

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ all:
 	#mkdir -p out
 	g++ -pthread -O3 -DNDEBUG -I . -I .. -isystem /usr/include -isystem /usr/include -isystem /usr/include -isystem /usr/include -DOPENVDB_USE_LOG4CPLUS -o out/levelset src/levelset.cpp -I . \
 	    -Wl,-rpath,/usr/local/openvdb/lib -L/usr/local/openvdb /usr/local/openvdb/libopenvdb.so.3.1.0 \
-	    -ldl -lm -lz -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lHalf -Wl,-rpath,/usr/lib -L/usr/lib -ltbb -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lboost_iostreams -lboost_system  -Wl,-rpath,/usr/lib -L/usr/lib -llog4cplus -Wl,-rpath,/usr/lib -L/usr/lib -lrt -ltbbmalloc_proxy -ltbbmalloc
+	    -ldl -lm -lz -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lHalf -Wl,-rpath,/usr/lib -L/usr/lib -ltbb -Wl,-rpath,/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lboost_iostreams -lboost_system  -Wl,-rpath,/usr/lib -L/usr/lib -llog4cplus -Wl,-rpath,/usr/lib -L/usr/lib -lrt -ltbbmalloc_proxy -ltbbmalloc -ggdb
+
 #run:
 #	vglrun vdb_view grids.vdb
 	
--- src/levelset.cpp
+++ src/levelset.cpp
@@ -4,16 +4,32 @@
 #include <openvdb/math/Coord.h>
 #include <openvdb/math/Vec3.h>
 #include <openvdb/openvdb.h>
+#include <openvdb/tools/Interpolation.h>
+#include <openvdb/tools/LevelSetAdvect.h>
 #include <openvdb/tools/LevelSetSphere.h>
+#include <openvdb/tools/VelocityFields.h>
 #include <openvdb/tree/TreeIterator.h>
 #include <openvdb/Types.h>
+#include <stddef.h>
 #include <vector>
-
-#include "/usr/local/openvdb/viewer/Viewer.h"
 
 using namespace openvdb;
 using namespace std;
-using namespace openvdb_viewer;
+
+FloatGrid::Ptr readGrid() {
+	io::File file("grids.vdb");
+	file.open();
+	GridBase::Ptr baseGrid;
+	baseGrid = file.readGrid("LevelSetSphere");
+
+	file.close();
+
+	// "LevelSetSphere" is a FloatGrid,
+	// so cast the generic grid pointer to a FloatGrid pointer.
+	FloatGrid::Ptr grid = gridPtrCast<FloatGrid>(baseGrid);
+
+	return grid;
+}
 
 float constantForce() {
 	//Force in Newtons
@@ -21,13 +37,22 @@ float constantForce() {
 	return k;
 }
 
-float gaussianForce() {
-	/// For bumps, inside and outside ///
-	float sigma = 1;
-//	F = exp ���||p���p 0 ||/2��	2
-	float k = 1;
-	return k;
-}
+/*float gaussianForce() {
+ /// For bumps, inside and outside ///
+ float sigma = 1;
+ float k = 1;
+ float mean = 1;
+
+ random_device rd;
+ mt19937 gen(rd());
+
+ // values near the mean are the most likely
+ // standard deviation affects the dispersion of generated values from the mean
+ normal_distribution<> d(mean, sigma);
+
+
+ return k;
+ }*/
 
 void createAndWriteGrid() {
 	float backgroundValue = 2.0;
@@ -37,7 +62,7 @@ void createAndWriteGrid() {
 
 	grid = tools::createLevelSetSphere<FloatGrid>(
 	/*radius=*/100.0, /*center=*/Vec3f(1.5, 2, 3),
-	/*voxel size=*/1.5, /*width=*/2	); //METERS
+	/*voxel size=*/1.5, /*width=*/2); //METERS
 
 	grid->setName("LevelSetSphere");
 
@@ -50,17 +75,37 @@ void createAndWriteGrid() {
 	file.close();
 }
 
+
+void writeGrid(FloatGrid::Ptr grid) {
+	io::File output_file("new_grids.vdb");
+
+	GridPtrVec grids;
+	grids.push_back(grid);
+
+	output_file.write(grids);
+	output_file.close();
+}
+
+
+float fetchGridValue(FloatGrid::Ptr grid, int x, int y, int z) {
+
+	// Request a value accessor for accelerated access.
+	// (Because value accessors employ a cache, it is important to declare
+	// one accessor per thread.)
+	FloatGrid::ConstAccessor accessor = (*grid).getConstAccessor();
+
+	// Instantiate the GridSampler template on the accessor type and on
+	// a box sampler for accelerated trilinear interpolation.
+	tools::GridSampler<FloatGrid::ConstAccessor, tools::BoxSampler> fastSampler(
+			accessor, (*grid).transform());
+
+	// Compute the value of the grid at a location in index space.
+	float worldValue = fastSampler.isSample(openvdb::Vec3R(x, y, z));
+	return worldValue;
+}
+
 void modifyGrid() {
-	io::File file("grids.vdb");
-	file.open();
-	GridBase::Ptr baseGrid;
-	baseGrid = file.readGrid("LevelSetSphere");
-
-	file.close();
-
-	// "LevelSetSphere" is a FloatGrid,
-	// so cast the generic grid pointer to a FloatGrid pointer.
-	FloatGrid::Ptr grid = gridPtrCast<FloatGrid>(baseGrid);
+	FloatGrid::Ptr grid = readGrid();
 
 	// Visit and update all of the grid's active values, which correspond to
 	// voxels on the narrow band.
@@ -96,46 +141,57 @@ void modifyGrid() {
 	 //construct a grid, display it.
 	 }
 	 */
-
-	io::File output_file("new_grids.vdb");
-
-	GridPtrVec grids;
-	grids.push_back(grid);
-
-	output_file.write(grids);
-	output_file.close();
+	writeGrid(grid);
 }
 
-void displayGrid() {}
 /*
-void displayGrid() {
-	//openvdb_viewer::Viewer viewer = openvdb_viewer::init("Test",
-	//false);
+ void displayGrid() {
+ //openvdb_viewer::Viewer viewer = openvdb_viewer::init("Test",
+ //false);
 
-	GridCPtrVec allGrids;
+ GridCPtrVec allGrids;
 
-	// Load VDB files.
-	io::File file("new_grids.vdb");
-	file.open();
+ // Load VDB files.
+ io::File file("new_grids.vdb");
+ file.open();
 
-	GridPtrVecPtr grids = file.getGrids();
-	allGrids.insert(allGrids.end(), grids->begin(), grids->end());
+ GridPtrVecPtr grids = file.getGrids();
+ allGrids.insert(allGrids.end(), grids->begin(), grids->end());
 
-	viewer.open();
-	viewer.view(allGrids);
+ viewer.open();
+ viewer.view(allGrids);
 
-	openvdb_viewer::exit();
+ openvdb_viewer::exit();
+ }
+ */
+
+
+void selectROI() {
+
 }
-*/
+
+void advectGrid() {
+	FloatGrid::Ptr grid = readGrid();
+	tools::DiscreteField<tools::StaggeredBoxSampler> (*field);
+
+	tools::LevelSetAdvection<FloatGrid, tools::DiscreteField, tools::InterruptT>(grid, field, NULL);
+	writeGrid(grid);
+
+}
 
 int main() {
 	// Initialize the OpenVDB library.  This must be called at least
 	// once per program and may safely be called multiple times.
 	initialize();
-	// Create an empty floating-point grid with background value 0.
-	createAndWriteGrid();
 
+	advectGrid();
+
+	// Create an empty floating-point grid with background value 0.
+//	createAndWriteGrid();
+
+//	createField();
 //	modifyGrid();
 //	displayGrid();
+//	selectROI();
 }
 
